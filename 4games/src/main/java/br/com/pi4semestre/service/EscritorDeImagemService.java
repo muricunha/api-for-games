6945687fe7d9aa2a51450af3262cc82011da4c4b
Lucas Freire <lucasflopes94@gmail.com>
2024-06-04 23:40:24 -0300

    ajustando mapeamento salvar imagem

--- 4games/src/main/java/br/com/pi4semestre/service/EscritorDeImagemService.java
+++ 4games/src/main/java/br/com/pi4semestre/service/EscritorDeImagemService.java
@@ -17,7 +17,7 @@ import static java.lang.String.format;
 @Service
 public class EscritorDeImagemService {
 
-    private static final String URL_BASE = System.getProperty("user.dir") + "\\angular\\angular-for-games\\src\\assets\\imagens-produtos\\";
+    private static final String URL_BASE = "C:\\Users\\Lucas\\Desktop\\PI 4 º Semestre\\angular-for-games\\src\\assets\\images\\";
 
     private String nomeArquivo;
 
